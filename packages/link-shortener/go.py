374c84100e53ae6f30d78df7bb68e132d7705e5a
Jamie Read <hey@jread.dev>
2023-10-21 17:12:21 +0100

    Implement simple link shortener

--- packages/link-shortener/go.py
+++ packages/link-shortener/go.py
@@ -1,5 +1,15 @@
+ROUTES = {
+  "goog": "httpsm://google.co.uk"
+}
+
 def main(args):
-  name = args.get("name", "stranger")
-  greeting = "Hello " + name + "!"
-  print(greeting)
-  return {"body": greeting}
+  name = args["http"]["path"]
+
+  redirect_url = ROUTES.get(name, "https://jread.dev")
+
+  return {
+    "statusCode": 302,
+    "headers": {
+      "Location": redirect_url
+    }
+  }
